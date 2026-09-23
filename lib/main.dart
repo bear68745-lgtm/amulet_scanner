adc4ad8260ce2e92518e81c56c68d04d034ec24d
bear68745-lgtm <bear68745@gmail.com>
2026-09-24 00:26:40 +0700

    Refactor import statements in main.dart
    Fixed import statements by removing duplicates and correcting syntax.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
+import 'pages/scan_page.dart';
 import 'dart:io';
-
 import 'package:camera/camera.dart';
-import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-
+import 'package:flutter/material.dart';importt 'package:image_picker/image_picker.dart';
 import 'models.dart';
 import 'scan_data.dart';
 import 'storage.dart';
